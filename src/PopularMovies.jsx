693efdd03bc870038516af4dc05ee805476107b4
Adrian Balbuena <abalbuena250@gmail.com>
2022-05-25 22:52:31 -0300

    create PopularMovies components

---
+++ src/PopularMovies.jsx
@@ -0,0 +1,14 @@
+import './PopularMovies.css';
+
+function PopularMovies() {
+  return (
+    <div className="container">
+      <ul>
+        <li>Esta peicula tiene un titulo suuuuuuupper laaaaaargo</li>
+        <li>Pelicula 2 </li>
+        <li>Pelicula 3 </li>
+      </ul>
+    </div>
+  );
+}
+export default PopularMovies;
---
+++ src/Title.jsx
@@ -0,0 +1,11 @@
+import './Title.css';
+
+function Title() {
+  return (
+    <div>
+      <h1 className="title">Cuevana10</h1>
+    </div>
+  );
+}
+
+export default Title;
